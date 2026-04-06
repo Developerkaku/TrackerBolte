73af220e46b8ce1b2613ce3938781dab220413f1
Karthik <karthikprofessional2404@gmail.com>
2026-04-06 18:26:01 +0530

    change tag to be unique

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 self.addEventListener('push', (event) => {
   const data = event.data.json();
-  
+
   const options = {
     body: data.body,
     icon: '/icon.png',
     badge: '/badge.png', // Small icon for the status bar
-    tag: 'urgent-alert', // Unique ID: new notifications with same tag replace old ones
+    tag: `urgent-alert: ${Date.now().toString()}`, // Unique ID: new notifications with same tag replace old ones
     renotify: true,      // Causes device to vibrate/ring even if tag is same
     vibrate: [500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170, 40],
     data: { url: data.url || '/' }
